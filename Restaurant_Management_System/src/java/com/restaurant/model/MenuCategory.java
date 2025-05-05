d51f442e6f422d49a480529e291f8eac8113ce2a
S.D.J Bhagya Nuwanadhara <kumuduelp@gmail.com>
2025-05-05 09:44:17 +0530

    Create DBConnection.java/model,InventoryItem.java/model,MenuCategory.java/model

--- Restaurant_Management_System/src/java/com/restaurant/model/MenuCategory.java
+++ Restaurant_Management_System/src/java/com/restaurant/model/MenuCategory.java
@@ -1,14 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.restaurant.model;
 
-/**
- *
- * @author Novaline
- */
 public class MenuCategory {
+    private int categoryId;
+    private String categoryName;
+    private String description;
     
+    //Constructors
+    public MenuCategory() {}
+    
+    public MenuCategory(int categoryId, String categoryName, String description) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+    
+    //Getters and Setters 
+    public int getCategoryId() {
+        return categoryId;
+    }
+    
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+    
+    public String getCategoryName() {
+        return categoryName;
+    }
+    
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+    
+    public String getDesciption() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    @Override
+    public String toString() {
+        return "MenuCategory{" +
+                "categoryId=" + categoryId +
+                ", categoryName'" + categoryName + '\'' +
+                ",description='" + description + '\''+
+                '}';
+    }
 }
