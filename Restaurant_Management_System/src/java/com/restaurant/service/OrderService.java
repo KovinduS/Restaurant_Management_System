d389b1689c1471790d761735e909247f632f4f86
wjkaushadha227 <jehanikaushada@gmail.com>
2025-05-09 09:19:43 +0530

    create orderService.java

--- Restaurant_Management_System/src/java/com/restaurant/service/OrderService.java
+++ Restaurant_Management_System/src/java/com/restaurant/service/OrderService.java
@@ -5,10 +5,62 @@
  */
 package com.restaurant.service;
 
+import com.restaurant.dao.OrderDAO;
+import com.restaurant.model.Order;
+import com.restaurant.model.OrderItem;
+import java.util.List;
+
 /**
  *
  * @author Novaline
  */
 public class OrderService {
-    private orderDAO orderDao;
+     private OrderDAO orderDao;
+     
+     public OrderService(){
+         this.orderDao = new OrderDAO();
+     }
+     
+     public int createOrder (Order order,List<OrderItem> items){
+         try{
+             int orderId = orderDao.createOrder(order);
+             if (orderId > 0){
+                 boolean itemsAdded = orderDao.addOrderItems(orderId, items);
+                 if (itemsAdded){
+                     return orderId;
+                 }
+             }
+             return -1;
+         }catch (Exception e){
+             e.printStackTrace();
+                     return -1;
+         }
+     }
+     
+     public List<Order> getActiveOrders() {
+         try{
+             return orderDao.getActiveOrders();
+         } catch (Exception e){
+             e.printStackTrace();
+             return null;
+         }
+     }
+     
+      public boolean updateOrderStatus(int orderId, String status) {
+          try{
+              return orderDao.updateOrderStatus(orderId, status) ;
+          }catch (Exception e) {
+              e.fillInStackTrace();
+              return false;
+          }
+      }
+      
+      public Order getOrderById(int orderId) {
+          try{
+              return orderDao.getOrderById(orderId);
+          }catch (Exception e){
+              e.printStackTrace();
+              return null;
+          }
+      }
 }
